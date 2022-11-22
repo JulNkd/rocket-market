8f94e5255791b0fd9c33bf12b3c97b31a7c8ea1e
Joy Klasen <joy.klasen@gmail.com>
2022-11-22 14:14:45 +0100

    profile page WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get 'profile', to: 'pages#profile'
 
   resources :rockets do
     resources :reservations, only: %i[new create delete show]
