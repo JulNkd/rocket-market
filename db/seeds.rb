84b9587a30d5ca19963e7319795c807df9944aa7
Julien Nakad <julien.nakad@gmail.com>
2022-11-24 16:08:36 +0100

    fixing heroku end input

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,30 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-require 'faker'
 
 Reservation.destroy_all
 Rocket.destroy_all
 User.destroy_all
-# first_user = User.create!(email: "toto@gmail.com", password: "azerty")
-# Rocket.create!(name: "Arianne", description: "Une fusée incroyable", user_id: "#{first_user.id}")
-
-puts "seed commence"
-
-20.times do
-  user = User.new(
-    email: Faker::Internet.email,
-    password: Faker::Internet.password
-  )
-  user.save!
-
-  rocket = Rocket.new(
-    name: Faker::Movies::StarWars.vehicle,
-    address: Faker::Movies::StarWars.planet,
-    description: Faker::Movies::StarWars.quote,
-    user_id: user.id
-  )
-  rocket.save!
-end
-
-puts "seed termine"
